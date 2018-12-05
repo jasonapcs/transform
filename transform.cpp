7aaf25aa0ee44d54cf58070513d6c5165442b55f
jasonapcs <42473083+jasonapcs@users.noreply.github.com>
2018-12-05 09:33:30 -0500

    Update transform.cpp

--- transform.cpp
+++ transform.cpp
@@ -3,3 +3,24 @@ ID: jason.z2
 PROG: transform
 LANG: C++
 */
+
+#include <fstream>
+#include <vector>
+#include <string>
+
+struct sqPattern {
+  public:
+  pattern(int sz, string items) : size(sz) {
+    pattern.resize(size);
+    int index = 0;
+    for (int i = 0; i < size; i++)
+      for (j = 0; j < size; i++){
+        pattern[i].push_back(items[index]);
+        index++;
+      }
+  }
+  
+  int size;
+  std::vector<std::vector<char>> pattern;
+  
+};
